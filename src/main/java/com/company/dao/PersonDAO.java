7b5dd5c8f265507f38ff60a2db6d471297aaa8bf
Valera Cidik <cidikvalera@gmail.com>
2022-10-15 12:23:27 +0300

    KRUD web application running with Hibernate

---
+++ src/main/java/com/company/dao/PersonDAO.java
@@ -0,0 +1,58 @@
+package com.company.dao;
+
+import com.company.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Component
+public class PersonDAO {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public PersonDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Person> index() {
+        Session session = sessionFactory.getCurrentSession();
+
+        List<Person> people = session.createQuery("select p from Person p", Person.class)
+                .getResultList();
+
+        return people;
+    }
+
+    @Transactional(readOnly = true)
+    public Person show(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Person.class, id);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
+    }
+
+    @Transactional
+    public void update(int id, Person updatedPerson) {
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.get(Person.class, id);
+        person.setAge(updatedPerson.getAge());
+        person.setName(updatedPerson.getName());
+    }
+
+    @Transactional
+    public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(session.get(Person.class, id));
+    }
+}
